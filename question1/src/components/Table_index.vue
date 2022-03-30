29926550e4213b92ae03b2499c90f335a7b6e7b4
Ruksana-500 <ruksana.shaik@500apps.com>
2022-03-30 16:51:49 +0530

    COMMIT2

---
+++ question1/src/components/Table_index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <center><table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Index</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="data in table_data" :key="data.id">
+                   <td>{{data.name}} </td>
+                   <td>{{data.index}}</td>
+                </tr>
+            </tbody>
+        </table></center>
+  </div>
+</template>
+<script>
+export default {
+  name: 'tableData',
+     data(){
+         return{
+              table_data : [
+                  { "name" :" Agile CRM" ,'index':'0', "description" :" A company of excellence"}, 
+                  {"name":"mantra technologies", 'index':'1',"description":"500apps" },
+                  {"name":"500apps Python", 'index':'2',"description":"Backend built using python" },
+                  {"name":"500apps infinity", 'index':'3',"description":"Builtusing vue JS" }
+                  ]
+
+
+    };
+  }
+ };
+</script>
+
+<style scoped>
+table,td,th{
+   border:1px solid;
+}
+</style>
