8e758c1ff0a29f22dc028271d6e304cfa45fe011
Ruksana-500 <ruksana.shaik@500apps.com>
2022-03-30 16:51:59 +0530

    COMMIT3

---
+++ question1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <tableindex/>
+  </div>
+</template>
+
+<script>
+import tableindex from '../src/components/table_index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    tableindex
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
